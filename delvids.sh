3f0fb0f66977e33436a619a56bbe3ff1f2e248ff
AnotherSamWilson <samwilson303@gmail.com>
2022-10-12 13:52:46 -0400

    No longer assume footage is stored in a folder called /videos

--- delvids.sh
+++ delvids.sh
@@ -2,9 +2,9 @@
 partition_memory_free=$(df -B K --output=avail $1 | tail -n 1)
 partition_memory_free=${partition_memory_free%?} # Removes the unit at the end
 while (( $partition_memory_free < $2)); do
-	delvid=$( ls ./videos -1t | grep footage_ | tail -1 )
+	delvid=$( ls $1 -1t | grep footage_ | tail -1 )
 	echo "deleting ${delvid}"
-	rm $1/videos/${delvid}
+	rm $1/${delvid}
 	# Get the total spaced used on the partition the videos are being stored
 	partition_memory_free=$(df -B K --output=avail $1 | tail -n 1)
 	partition_memory_free=${partition_memory_free%?}
